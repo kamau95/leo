e516e55772b84dd7c94b760e7ba8669e96ca59c7
linus kiogora <linuskamau586@gmail.com>
2023-04-02 12:14:51 +0300

    leo change

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App( ) {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(87);
+  //handling state zero
 const handleClick= ( )=>{
   setCount( prevCount=>prevCount+1)
 }
 const handleDecrement = ( )=> {
   setCount( prevCount=>prevCount-1)
 }
+
+
   return (
     <div className="App">
-      
+    <div>
       <button onClick={handleClick}>Click Me</button>
       <h1>the current count is {count} </h1>
       <button onClick={handleDecrement}>Click Me</button>
-    </div>
+      </div>
+      <div>
+      <button onClick={( )=>setValue(value+1)}>+</button>
+      <h1>the value is {value}</h1>
+      <button onClick={( )=>setValue(value-1)}>-</button>
+      </div>
+      <div>
+      <button onClick={( )=>setValue(87)}>Reset</button>
+      </div>
+  </div>
   )
 }
 
